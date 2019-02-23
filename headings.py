0c3ebe7a2558367f8d3abb27503cf0f798cbd3a4
Matt Lane <lane.matthew@gmail.com>
2019-02-23 15:48:13 -0800

    adding column headings for csv

---
+++ headings.py
@@ -0,0 +1,7 @@
+headings = [
+    "year", "state", "state_code", "jurisdiction", "precincts",
+    "polling_places", "poll_workers", "voting_age_pop",
+    "citizen_voting_age_pop", "registration", "ballots", "absentee_requested",
+    "absentee_returned", "absentee_counted", "provisional_cast",
+    "provisional_counted"
+]
