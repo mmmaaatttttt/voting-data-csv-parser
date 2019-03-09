a8f907e29b82bb62b8131c4502ac2f94f48ade50
Matt Lane <lane.matthew@gmail.com>
2019-03-09 14:28:37 -0800

    moving csv_utils

---
+++ csv_utils.py
@@ -0,0 +1,69 @@
+headings = [
+    "year", "state", "state_code", "jurisdiction", "active_registration",
+    "precincts", "polling_places", "poll_workers", "worker_age_group_1",
+    "worker_age_group_2", "worker_age_group_3", "worker_age_group_4",
+    "worker_age_group_5", "worker_age_group_6", "poll_worker_difficulty",
+    "election_participants"
+]
+
+workbook_data = [{
+    "year": 2016,
+    "sheets": [{
+        "label": "SECTION A",
+        "columns": [1, 2, 8]
+    }, {
+        "label": "SECTION D",
+        "columns": [3, 5, 15, 17, 18, 19, 20, 21, 22, 24]
+    }, {
+        "label": "SECTION F",
+        "columns": [3]
+    }]
+}, {
+    "year": 2014,
+    "sheets": [{
+        "label": "EAVS_Section_A",
+        "columns": [2, 3, 9]
+    }, {
+        "label": "EAVS_Section_D",
+        "columns": [4, 6, 17, 19, 20, 21, 22, 23, 24, 27]
+    }, {
+        "label": "EAVS_Section_F",
+        "columns": [4]
+    }]
+}, {
+    "year": 2012,
+    "sheets": [{
+        "label": "2012NVRAData",
+        "columns": [0, 1, 8]
+    }, {
+        "label": "Section D",
+        "columns": [3, 5, 16, 18, 19, 20, 21, 22, 23, 26]
+    }, {
+        "label": "Section F",
+        "columns": [3]
+    }]
+}, {
+    "year": 2010,
+    "sheets": [{
+        "label": "Section A",
+        "columns": [0, 1, 8]
+    }, {
+        "label": "Section D",
+        "columns": [3, 5, 16, 18, 19, 20, 21, 22, 23, 26]
+    }, {
+        "label": "Section F",
+        "columns": [3]
+    }]
+}, {
+    "year": 2008,
+    "sheets": [{
+        "label": "SectionA",
+        "columns": [3, 1, 10]
+    }, {
+        "label": "SectionD",
+        "columns": [4, 7, 22, 25, 26, 27, 28, 29, 30, 33]
+    }, {
+        "label": "SectionF",
+        "columns": [4]
+    }]
+}]
--- headings.py
+++
@@ -1,7 +0,0 @@
-headings = [
-    "year", "state", "state_code", "jurisdiction", "active_registration",
-    "precincts", "polling_places", "poll_workers", "worker_age_group_1",
-    "worker_age_group_2", "worker_age_group_3", "worker_age_group_4",
-    "worker_age_group_5", "worker_age_group_6", "poll_worker_difficulty",
-    "election_participants"
-]
