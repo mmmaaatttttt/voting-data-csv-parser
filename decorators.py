ed33c4ba5ac191c3889c7156692dc0faeba73e44
Matt Lane <lane.matthew@gmail.com>
2019-03-09 14:32:08 -0800

    modifying decorator

--- decorators.py
+++ decorators.py
@@ -4,7 +4,8 @@ from functools import wraps
 def log_start_and_end(fn):
     @wraps(fn)
     def wrapped_fn(*args, **kwargs):
-        print(f"Running {fn.__name__}...")
+        year = kwargs.get("year")
+        print(f"Running {fn.__name__} with year={year}")
         output = fn(*args, **kwargs)
         print(f"{fn.__name__} complete.")
         return output
